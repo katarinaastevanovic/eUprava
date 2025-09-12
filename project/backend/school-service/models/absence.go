6bb7065216dedf978e92a853627b55a5f2741beb
Katarina Stevanovic <katarina9stevanovic@gmail.com>
2025-09-12 17:11:31 +0200

    models added

---
+++ project/backend/school-service/models/absence.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Absence struct {
+	gorm.Model
+	Type    AbsenceType `gorm:"type:varchar(20);not null"`
+	Date    time.Time   `gorm:"not null"`
+	Student Student     `gorm:"foreignKey:StudentID"`
+	Subject Subject     `gorm:"foreignKey:SubjectID"`
+}
