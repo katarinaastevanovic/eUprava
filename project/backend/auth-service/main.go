505adb27b1a9c67130c55af49c7ed6ffe0ac9b34
Katarina Stevanovic <katarina9stevanovic@gmail.com>
2025-09-15 18:15:39 +0200

    docker

--- project/backend/auth-service/main.go
+++ project/backend/auth-service/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	port := "8080"
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Auth service is running!")
+	})
+
+	log.Printf("🚀 Auth service listening on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
